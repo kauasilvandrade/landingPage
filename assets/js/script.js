291e6171f7d3b0dd1e1ff7c47d29c893bc64e113
kauasilvandrade <kauaa422@gmail.com>
2024-03-13 14:28:31 -0300

    Alterações no cabeçalho

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,31 @@
-let cabecalho = document.querySelector('.secaoTopoCabecalho');
+// ABRIR E FECHAR MENU
 let btnMenu = document.getElementById('secaoTopo__btnMenu');
-
 function menuShow(event) {
     
     // MENU
     let listaMenu = document.querySelector('.secaoTopoCabecalho__listaMenu');
     listaMenu.classList.toggle('ativado');
-
+    
+    // BOTÃO MENU
+    btnMenu.classList.toggle('ativado');
+    
     // ACESSIBILIDADE
     if (event.type == 'touchstart') event.preventDefault();    
-
+    
     const menuAtivado = listaMenu.classList.contains('ativado');
-
+    
     event.currentTarget.setAttribute('aria-expanded', menuAtivado)
-
+    
     if (menuAtivado) {
         event.currentTarget.setAttribute('aria-label', 'Botão fechar menu')
     } else {
         event.currentTarget.setAttribute('aria-label', 'Botão abrir menu')
     }
-
+    
 }
-
 btnMenu.addEventListener('click', menuShow)
 btnMenu.addEventListener('touchstart', menuShow)
 
-
-// document.addEventListener('scroll', function() {
-    
-//     let pos = 1173;
-//     if (window.pageYOffset >= 1063) {
-//         cabecalho.classList.add('scroll')
-//     } else {
-//         cabecalho.classList.remove('scroll')
-//         btnMenu.classList.remove('ativado');
-//         listaMenu.classList.remove('ativado');
-//     }
-
-// })
+// MOSTRAR NOVO CABEÇALHO AO SER SCROLADO
+let cabecalho = document.querySelector('.secaoTopoCabecalho');
+window.addEventListener('scroll', () => cabecalho.classList.toggle('scroll', window.scrollY > 50));
