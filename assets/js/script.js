af6c3ef6e16adcd79dad59501c252c36b6f1060f
kauasilvandrade <kauaa422@gmail.com>
2024-03-13 11:11:59 -0300

    Deixando botão acessivel de menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,40 @@
-let cabecalho = document.querySelector('.secaoTopoCabecalho')
+let cabecalho = document.querySelector('.secaoTopoCabecalho');
 let btnMenu = document.getElementById('secaoTopo__btnMenu');
-let listaMenu = document.querySelector('.secaoTopoCabecalho__listaMenu');
 
-btnMenu.addEventListener('click', function() {
-    btnMenu.classList.toggle('ativado');
-    listaMenu.classList.toggle('ativado');
-})
-
-
-document.addEventListener('scroll', function() {
+function menuShow(event) {
     
-    let pos = 1063;
-    if (window.pageYOffset >= 1063) {
-        cabecalho.classList.add('scroll')
+    // MENU
+    let listaMenu = document.querySelector('.secaoTopoCabecalho__listaMenu');
+    listaMenu.classList.toggle('ativado');
+
+    // ACESSIBILIDADE
+    if (event.type == 'touchstart') event.preventDefault();    
+
+    const menuAtivado = listaMenu.classList.contains('ativado');
+
+    event.currentTarget.setAttribute('aria-expanded', menuAtivado)
+
+    if (menuAtivado) {
+        event.currentTarget.setAttribute('aria-label', 'Botão fechar menu')
     } else {
-        cabecalho.classList.remove('scroll')
-        btnMenu.classList.remove('ativado');
-        listaMenu.classList.remove('ativado');
+        event.currentTarget.setAttribute('aria-label', 'Botão abrir menu')
     }
 
-})
+}
+
+btnMenu.addEventListener('click', menuShow)
+btnMenu.addEventListener('touchstart', menuShow)
+
+
+// document.addEventListener('scroll', function() {
+    
+//     let pos = 1173;
+//     if (window.pageYOffset >= 1063) {
+//         cabecalho.classList.add('scroll')
+//     } else {
+//         cabecalho.classList.remove('scroll')
+//         btnMenu.classList.remove('ativado');
+//         listaMenu.classList.remove('ativado');
+//     }
+
+// })
